ed8b11f3fb19f3976df2330e1f1e1d137687486c
Deep Kansara <deepkansara1212@gmail.com>
2025-04-28 10:13:02 +0530

    Conditionals

---
+++ 04_Conditionals/01_If.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int num1 = 12, num2 = 45; 
+
+    if(num1 > num2) {
+        // Code Logic
+        cout << "num1 is greater than num2" << endl;
+    }
+    else {
+        cout << "num1 is less than num2" << endl;
+    }
+
+    return 0;
+}
---
+++ 04_Conditionals/03_Ternary-Operator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int time = 20;
+    // if (time < 18)
+    // {
+    //     cout << "Good day.";
+    // }
+    // else
+    // {
+    //     cout << "Good evening.";
+    // }
+
+    string result = (time < 18) ? "Good day." : "Good evening.";
+    return 0;
+}
